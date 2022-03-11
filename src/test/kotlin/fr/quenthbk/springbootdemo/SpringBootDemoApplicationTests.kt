447af89d34a90c12ead1d822992d809b00d6336f
quenthbk <quenthbk@gmail.com>
2022-03-11 19:38:44 +0100

    first commit

---
+++ src/test/kotlin/fr/quenthbk/springbootdemo/SpringBootDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package fr.quenthbk.springbootdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
